6ca4caa464bdda4c543d89acc46cfc767e2192fd
RJ Bergeron <rbergero@gmail.com>
2017-03-14 03:12:47 -0400

    include build time here since we ingest upstream artifacts wildly

--- efw-files/stamp.sh
+++ efw-files/stamp.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 BUILD_STR=""
+HUMANTIME=$(date -r $BUILD_TS)
 
 if [ -z "$BUILD_SHA" ] ; then
   echo "BUILD_SHA not in environment, aborting" 1>&2
@@ -8,7 +9,8 @@ if [ -z "$BUILD_SHA" ] ; then
 else
   BUILD_STR="Build $BUILD_SHA"
   echo "Building EFW commit $BUILD_SHA"
+  echo "(Timestamp $HUMANTIME)"
 fi
 
 # replace im= attribute in getty with build info
-sed -i.dist -e 's/im=.*:/im=\\r\\n\%s\/\%m EFW '"$BUILD_STR"' (\\%t)\\r\\n\\r\\n:/' /etc/gettytab
+sed -i.dist -e 's/im=.*:/im=\\r\\n\%s\/\%m EFW '"$BUILD_STR"'\\r\\nBuilt '"$HUMANTIME"' (\\%t)\\r\\n\\r\\n:/' /etc/gettytab
