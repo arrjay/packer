9fca7426f78e15ea8d9f67b750abbcd11a334740
RJ Bergeron <rbergero@gmail.com>
2017-03-14 04:03:18 -0400

    error checking, reboot when done here

--- efw-files/openup.sh
+++ efw-files/openup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 # install openup ( https://www.mtier.org/solutions/apps/openup/ )
 ftp -o /usr/local/sbin/openup http://$PACKER_HTTP_ADDR/redist/openup
@@ -6,3 +7,6 @@ chmod +x /usr/local/sbin/openup
 
 # do a run now
 /usr/local/sbin/openup
+
+# reboot
+shutdown -r now
