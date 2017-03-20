cebdd916c993f706539ecf9f8b2df8359d1e354e
RJ Bergeron <rbergero@gmail.com>
2017-03-20 12:06:34 -0400

    supply arguments to qemu to actually make it fast, drop gui

--- test/wcs.sh
+++ test/wcs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 qemu-img create -f qcow2 output-qemu-wcs/data.qcow2 36G
-qemu-system-x86_64 -net nic,model=virtio -net user -drive file=output-qemu-wcs/system.qcow2,if=virtio,snapshot=on -drive file=output-qemu-wcs/data.qcow2,if=virtio,snapshot=on
+qemu-system-x86_64 -machine accel=kvm -net nic,model=virtio -net user -drive file=output-qemu-wcs/system.qcow2,if=virtio,snapshot=on -drive file=output-qemu-wcs/data.qcow2,if=virtio,snapshot=on -nographic
