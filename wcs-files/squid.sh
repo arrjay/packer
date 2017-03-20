a8533a5b5985c9e698deae8b87d64fdcecc367cb
RJ Bergeron <rbergero@gmail.com>
2017-03-19 21:49:16 -0400

    add systemd unit for squid data volume

--- wcs-files/squid.sh
+++ wcs-files/squid.sh
@@ -8,3 +8,13 @@ fi
 set -e
 
 yum -y install squid
+
+echo "creating squid requires overrides"
+mkdir -p /etc/systemd/system/squid.service.requires
+
+echo "installing data volume service"
+mv /tmp/datavol.sh /usr/local/sbin
+chmod 0750 /usr/local/sbin/datavol.sh
+cp /tmp/datavol.unit /etc/systemd/system/squid-datavol.service
+chmod 0644 /etc/systemd/system/squid-datavol.service
+ln -s /etc/systemd/system/squid-datavol.service /etc/systemd/system/squid.service.requires/squid-datavol.service
