988f31e762038d84c18455582f191609395cbe6e
RJ Bergeron <rbergero@gmail.com>
2017-03-19 23:54:36 -0400

    squid configuration for wcs - m.k.o for the moment.

--- wcs-files/squid.sh
+++ wcs-files/squid.sh
@@ -18,3 +18,12 @@ chmod 0750 /usr/local/sbin/datavol.sh
 cp /tmp/datavol.unit /etc/systemd/system/squid-datavol.service
 chmod 0644 /etc/systemd/system/squid-datavol.service
 ln -s /etc/systemd/system/squid-datavol.service /etc/systemd/system/squid.service.requires/squid-datavol.service
+
+echo "configuring and enabling squid"
+mv -f /tmp/squid.conf /etc/squid/squid.conf
+chmod 0644 /etc/squid/squid.conf
+restorecon /etc/squid/squid.conf
+ln -s /usr/lib/systemd/system/squid.service /etc/systemd/system/multi-user.target.wants/squid.service
+
+echo "updating /etc/hosts"
+echo "127.0.0.1 mko.wcs.bbxn.us" >> /etc/hosts
