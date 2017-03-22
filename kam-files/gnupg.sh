f3b405d5d2a0a85f1b53414a772687d3ec2df60a
RJ Bergeron <rbergero@gmail.com>
2017-03-22 12:29:02 -0400

    install gnupg2

---
+++ kam-files/gnupg.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+GPG2=$(pkg_info -Q gnupg | grep ^gnupg-2)
+pkg_add $GPG2
--- test/kam.sh
+++ test/kam.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 qemu-img create -f qcow2 -b system.img output-qemu-kam/transient.qcow2
-qemu-system-x86_64 -machine accel=kvm -net nic,model=virtio,vlan=2 -drive file=output-qemu-kam/transient.qcow2,if=virtio,snapshot=on -display vnc=127.0.0.1:28 -chardev stdio,id=char0 -mon chardev=char0,mode=readline,default
+qemu-system-x86_64 -machine accel=kvm -net nic,model=virtio -net user -drive file=output-qemu-kam/transient.qcow2,if=virtio,snapshot=on -display vnc=127.0.0.1:28 -chardev stdio,id=char0 -mon chardev=char0,mode=readline,default
