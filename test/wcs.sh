f175d08f9a96d1c36f08629ec50689ba3c042639
RJ Bergeron <rbergero@gmail.com>
2017-03-22 03:50:01 -0400

    use transient images correctly

--- test/wcs.sh
+++ test/wcs.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 qemu-img create -f qcow2 output-qemu-wcs/data.qcow2 36G
-qemu-system-x86_64 -machine accel=kvm -net nic,model=virtio -net user -drive file=output-qemu-wcs/system.qcow2,if=virtio,snapshot=on -drive file=output-qemu-wcs/data.qcow2,if=virtio,snapshot=on -nographic
+qemu-img create -f qcow2 -b system.qcow2 output-qemu-wcs/transient.qcow2
+qemu-system-x86_64 -machine accel=kvm -net nic,model=virtio -net user -drive file=output-qemu-wcs/transient.qcow2,if=virtio,snapshot=on -drive file=output-qemu-wcs/data.qcow2,if=virtio,snapshot=on -nographic
+rm output-qemu-wcs/transient.qcow2
+rm output-qemu-wcs/data.qcow2
