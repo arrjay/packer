b96c80fbcd815dbf9e4d417078f507303841d2b9
RJ Bergeron <rbergero@gmail.com>
2017-03-22 02:49:30 -0400

    reorganize efw files to shared vs just-efw.

--- shared/openbsd/openup.sh
+++ shared/openbsd/openup.sh
@@ -2,7 +2,7 @@
 set -e
 
 # install openup ( https://www.mtier.org/solutions/apps/openup/ )
-ftp -o /usr/local/sbin/openup http://$PACKER_HTTP_ADDR/redist/openup
+cp /tmp/openup /usr/local/sbin
 chmod +x /usr/local/sbin/openup
 
 # do a run now
