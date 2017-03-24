a4643b43146808c82eb5711e2601f229bbba149a
RJ Bergeron <rbergero@gmail.com>
2017-03-24 03:14:11 -0400

    install a login manager to do our bidding, startx gets mad

--- kam-files/xinit.sh
+++ kam-files/xinit.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-# start X via ttys so we just, you know, are.
-sed -e 's@^ttyC4.*@ttyC4 "/usr/X11R6/bin/startx -- :0" xorg on@' -i /etc/ttys
+set -e
 
-# set the background to be less awful.
-sed -e 's@# start some.*@xsetroot -solid crimson@' -i /etc/X11/xinit/xinitrc
+pkg_add slim
 
-# modify xinitrc to put keyboard focus back
-printf '# force keyboard focus to vt4\nwsconsctl display.focus=4\n' >> /etc/X11/xinit/xinitrc
+echo "numlock off" >> /etc/slim.conf
+echo "default_user root" >> /etc/slim.conf
+echo "auto_login yes" >> /etc/slim.conf
+
+cp /etc/X11/xinit/xinitrc /root/.xinitrc
+
+sed -e 's@# start some.*@xsetroot -solid crimson@' -i /root/.xinitrc
+
+rcctl enable slim
