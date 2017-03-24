dbe21232099c801986ff568feda17fa42aa63151
RJ Bergeron <rbergero@gmail.com>
2017-03-24 00:58:50 -0400

    make x11 (1) work (2) slightly less eye-bleedy.

--- kam-files/xinit.sh
+++ kam-files/xinit.sh
@@ -2,3 +2,9 @@
 
 # start X via ttys so we just, you know, are.
 sed -e 's@^ttyC4.*@ttyC4 "/usr/X11R6/bin/startx -- :0" xorg on@' -i /etc/ttys
+
+# set the background to be less awful.
+sed -e 's@# start some.*@xsetroot -solid crimson@' -i /etc/X11/xinit/xinitrc
+
+# modify xinitrc to put keyboard focus back
+printf '# force keyboard focus to vt4\nwsconsctl display.focus=4\n' >> /etc/X11/xinit/xinitrc
