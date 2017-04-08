cd846f3b29d57aee513c4d3e29b04d400fad9488
RJ Bergeron <rbergero@gmail.com>
2017-04-08 15:42:48 -0700

    rewire openbsd mirroring to use wcs this time

--- shared/openbsd/environs.sh
+++ shared/openbsd/environs.sh
@@ -3,4 +3,5 @@
 set -e
 
 # configure pkg mirror
-echo "installpath = $MIRROR" >> /etc/pkg.conf
+echo "installpath = http://$MIRROR/pub/OpenBSD/$(uname -r)/packages/$(machine -a)" > /etc/pkg.conf
+echo "http://$MIRROR/pub/OpenBSD/" > /etc/installurl
