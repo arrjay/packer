84691da7316a575d759a57d0c0c3bb22f846b32d
RJ Bergeron <rbergero@gmail.com>
2017-04-23 19:50:34 -0400

    support virtio or esx for transit interface

--- efw-files/transit-if.sh
+++ efw-files/transit-if.sh
@@ -2,8 +2,14 @@
 
 set -e
 
+case "${PACKER_BUILDER_TYPE}" in
+  qemu)		ifname=vio1 ;;
+  vmware*)	ifname=vmx1 ;;
+  *)		false ;;
+esac
+
 {
   printf 'inet %s\n' "${TRANSIT_IP}"
   printf '-inet6\n'
   printf 'group transit\n'
-} > /etc/hostname.vio1
+} > /etc/hostname.$ifname
