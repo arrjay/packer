0d5ac4d7044ee3deb39bef62e441aa5dd11f20f6
RJ Bergeron <rbergero@gmail.com>
2017-04-23 15:30:46 -0400

    trickery to emulate a Real Vault

--- build.sh
+++ build.sh
@@ -5,6 +5,12 @@ export ATLAS_BUILD_GITHUB_COMMIT_SHA=$(git rev-parse HEAD)
 export ATLAS_BUILD_GITHUB_TAG=$(git describe --exact-match HEAD 2> /dev/null)
 export ATLAS_BUILD_SLUG="arrjay/infra"
 export BUILD_TIMESTAMP=$(date +%s)
+export PASSWORD_STORE_DIR=$(pwd)/vault
+
+for x in ${PASSWORD_STORE_DIR}/*.gpg ; do
+  name=$(basename "${x}" .gpg)
+  declare -x ${name}=$(pass ls ${name})
+done
 
 if [ -z "${MIRRORS_KERNEL_ORG}" ] ; then
   echo "setting MIRRORS_KERNEL_ORG"
