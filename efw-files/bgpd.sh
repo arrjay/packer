a128e2b2903fe22c5599355cca5ddeec5197b0f6
RJ Bergeron <rbergero@gmail.com>
2017-04-24 00:26:17 -0400

    started teaching efw how to bgp

---
+++ efw-files/bgpd.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+[ ! -z "${AS_NUMBER}" ]
+[ ! -z "${TRANSIT_IP}" ]
+[ ! -z "${BGP_ROUTER_ID}" ]
+
+pkg_add sipcalc
+
+subnet=$(sipcalc ${TRANSIT_IP}|awk -F'- ' '$0 ~ "Network address" { print $2 }')
+cidr=$(basename ${TRANSIT_IP})
+listener=$(echo ${TRANSIT_IP}|sed 's@/'${cidr}'@@')
+
+{
+  printf 'AS %s\n' "${AS_NUMBER}"
+  printf 'router-id %s\n\n' "${BGP_ROUTER_ID}"
+  printf 'nexthop qualify via bgp\n\n'
+  printf 'listen on %s\n\n' "${listener}"
+  printf 'neighbor %s/%s {\n' "${subnet}" "${cidr}"
+  printf ' local-address %s\n' "${listener}"
+  printf ' announce IPv4 unicast\n'
+  printf ' announce default-route\n'
+  printf '}\n'
+} > /etc/bgpd.conf
+
+chmod a-r /etc/bgpd.conf
+
+rcctl enable bgpd
