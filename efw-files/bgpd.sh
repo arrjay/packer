cf9ce14aeba25cd8d843a786dc3ceea97112dba8
RJ Bergeron <rbergero@gmail.com>
2017-04-24 03:45:33 -0400

    securing bgp sessions on openbsd is a fantastic excercise in frustration.

--- efw-files/bgpd.sh
+++ efw-files/bgpd.sh
@@ -5,7 +5,6 @@ set -e
 [ ! -z "${AS_NUMBER}" ]
 [ ! -z "${TRANSIT_IP}" ]
 [ ! -z "${BGP_ROUTER_ID}" ]
-[ ! -z "${BGP_MD5_PASS}" ]
 
 pkg_add sipcalc
 
@@ -19,11 +18,8 @@ listener=$(echo ${TRANSIT_IP}|sed 's@/'${cidr}'@@')
   printf 'nexthop qualify via bgp\n\n'
   printf 'listen on %s\n\n' "${listener}"
   printf 'neighbor %s/%s {\n' "${subnet}" "${cidr}"
-  printf ' local-address %s\n' "${listener}"
   printf ' announce IPv4 unicast\n'
   printf ' announce default-route\n'
-  printf ' tcp md5sig password %s\n' "${BGP_MD5_PASS}"
-  printf ' ttl-security yes\n'
   printf '}\n'
 } > /etc/bgpd.conf
 
