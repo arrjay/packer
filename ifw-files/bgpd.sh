665acc6bf7a06580f402d2f0767fa1f5beafdac4
RJ Bergeron <rbergero@gmail.com>
2017-04-24 04:01:45 -0400

    this lets ifw talk to efw via bgp, yay

---
+++ ifw-files/bgpd.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+[ ! -z "${AS_NUMBER}" ]
+[ ! -z "${INTERNAL_EFW_IP}" ]
+[ ! -z "${BGP_ROUTER_ID}" ]
+
+pkg_add sipcalc
+
+rcv_cidr=$(basename ${INTERNAL_EFW_IP})
+receiver=$(echo ${INTERNAL_EFW_IP}|sed 's@/'${rcv_cidr}'@@')
+
+{
+  printf 'AS %s\n' "${AS_NUMBER}"
+  printf 'router-id %s\n\n' "${BGP_ROUTER_ID}"
+  printf 'network inet connected\n\n'
+  printf 'nexthop qualify via bgp\n\n'
+  printf 'neighbor %s {\n' "${receiver}"
+  printf ' remote-as %s\n' "${AS_NUMBER}"
+  printf ' announce IPv4 unicast\n'
+  printf '}\n'
+} > /etc/bgpd.conf
+
+chmod a-r /etc/bgpd.conf
+
+rcctl enable bgpd
