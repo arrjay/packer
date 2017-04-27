9806870a674a5d88b40376c488f067fd8172c0f0
RJ Bergeron <rbergero@gmail.com>
2017-04-27 00:36:07 -0400

    write down how far I got with cbb...

--- cbb-files/consul-build.sh
+++ cbb-files/consul-build.sh
@@ -2,19 +2,15 @@
 
 set -e
 
-pkg_add go gmake
+pkg_add go gmake git bash
+
+go get github.com/mitchellh/gox
 
 mkdir -p /usr/local/src
-mkdir -p /usr/local/dist
-ftp -v -o /usr/local/dist/consul-v0.8.1.tar.gz https://github.com/hashicorp/consul/archive/v0.8.1.tar.gz
-sha512=$(sha512 /usr/local/dist/consul-v0.8.1.tar.gz | cut -d= -f2)
-set +e
-if [ "${sha512}" != " 484fb75f712c29571be5e7fb2f60cd70c8314392771e84756103571d809bb47212aa8572c7276b5e8db933ce48dc50662af89902ba997666fe49de52c8fa0497" ] ; then
-  echo "bad sum for consul-v0.8.1.tar.gz, got ${sha512}" 1>&2
-  exit 1
-fi
-set -e
-tar xzf /usr/local/dist/consul-v0.8.1.tar.gz -C /usr/local/src
-cd /usr/local/src/consul-0.8.1
+cd /usr/local/src
+git clone https://github.com/hashicorp/consul
+cd consul
+git checkout v0.8.1
+patch -p1 < /tmp/consul-build.patch
 gmake
 false
