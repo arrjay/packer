a74f32c88fc6fd308efef3dc2ed1a75c817df619
RJ Bergeron <rbergero@gmail.com>
2017-05-07 15:07:53 -0400

    vmm for dnc

--- dnc-files/interfaces.sh
+++ dnc-files/interfaces.sh
@@ -8,12 +8,10 @@ case "${PACKER_BUILDER_TYPE}" in
   *)		false ;;
 esac
 
+gw=$(dirname "${DEFAULTGW}")
+
 {
   printf 'inet %s\n' "${IPADDR}"
   printf '-inet6\n'
+  printf '!route add default %s\n' "${gw}"
 } > /etc/hostname.$ifname
-
-gw=$(dirname "${DEFAULTGW}")
-
-
-printf '%s\n' "${gw}" > /etc/mygate
