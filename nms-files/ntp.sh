80ccef932dfe197092e638ebde7cdb828919a9e9
RJ Bergeron <rbergero@gmail.com>
2017-05-08 02:04:51 -0400

    configure nms to re-serve ntp

---
+++ nms-files/ntp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+# configure chrony to advertise time services
+sed -i -e 's/^server.*//g' /etc/chrony.conf # first, remove static servers in favor of vmm
+
+# bind to all interfaces, allow all ips (the firewall will restrict where this works)
+{
+  printf 'bindaddress 0.0.0.0\n'
+  printf 'allow\n'
+} >> /etc/chronyd.conf
