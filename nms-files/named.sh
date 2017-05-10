f4edad5d506ef0918194f3d1549e22f42e55970e
RJ Bergeron <rbergero@gmail.com>
2017-05-10 04:01:42 -0400

    start named setup by wiring in an nsupdate key...

--- nms-files/named.sh
+++ nms-files/named.sh
@@ -1,5 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
 
+[ ! -z "${NMS1_DNS_KEY_TYPE}" ]
+[ ! -z "${NMS1_DNS_KEY_DATA}" ]
+
 yum -y install bind
+
+{
+  printf 'key nms1. {\n'
+  printf '  algorithm %s;\n' "${NMS1_DNS_KEY_TYPE//_/-}"
+  printf '  secret "'
+  # print out 56 character chunks
+  slide=0
+  set +e
+  while [ $((slide + 56)) -lt ${#NMS1_DNS_KEY_DATA} ] ; do
+    set -e
+    printf '%s ' "${NMS1_DNS_KEY_DATA:${slide}:56}"
+    slide=$((slide + 56))
+    set +e
+  done
+  set -e
+  printf '%s";\n' "${NMS1_DNS_KEY_DATA:${slide}}"
+  printf '};\n'
+} > /etc/named.update.key
