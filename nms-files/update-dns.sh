c24f101a0b72aa1271e25c1baedb8c4972a0a607
RJ Bergeron <rbergero@gmail.com>
2017-05-14 01:26:39 -0400

    rearrange update-dns to fit in a selinux straightjacket.

--- nms-files/update-dns.sh
+++ nms-files/update-dns.sh
@@ -12,4 +12,9 @@ keyname="nms1."
   printf 'NS=127.0.0.1\n'
 } > /usr/local/etc/update-dns.conf
 
-mv /tmp/update-dns /usr/local/sbin/update-dns && chmod 0755 /usr/local/sbin/update-dns
+# https://bugzilla.redhat.com/show_bug.cgi?id=1301854
+install -D -m 0755 /tmp/update-dns /etc/dhcp/scripts/update-dns && rm /tmp/update-dns
+restorecon -R /etc/dhcp/scripts
+
+# https://bugzilla.redhat.com/show_bug.cgi?id=1349044
+chgrp dhcpd /etc/dhcp
