4e57ed5b66699fe4d1e4c29d64a04768b3a23f4c
RJ Bergeron <rbergero@gmail.com>
2017-05-20 16:14:46 -0400

    slow bgpd check for efw to 60s

--- efw-files/consul.sh
+++ efw-files/consul.sh
@@ -4,6 +4,8 @@ set -e
 
 pkg_add consul jq
 
+mkdir -p /usr/local/etc
+
 {
   printf '{\n'
   printf '  "bootstrap_expect": 1,\n'
@@ -14,7 +16,34 @@ pkg_add consul jq
   printf '}\n'
 } > /etc/consul.d/config.json
 
-jq . < /etc/consul.d/config.json > /dev/null
+# we check bgpd here via a script because raw tcp checks cause bgpd to whine
+{
+  printf 'listener=%s\n' $(dirname "${TRANSIT_IP}")
+} > /usr/local/etc/efw-check.conf
+
+{
+  printf '{\n'
+  printf '  "service": {\n'
+  printf '    "name": "bgpd",\n'
+  printf '    "tags": [ "efw" ],\n'
+  printf '    "port": 179,\n'
+  printf '    "check": {\n'
+  printf '      "script": "/usr/local/libexec/consul-checks/efw-bgpd.sh",\n'
+  printf '      "interval": "60s"\n'
+  printf '    }\n'
+  printf '  }\n'
+  printf '}\n'
+  printf '\n'
+} > /etc/consul.d/bgpd.json
+
+for j in /etc/consul.d/*.json ; do
+  jq . < ${j} > /dev/null
+done
+
+mkdir -p /usr/local/libexec/consul-checks
+cp /tmp/efw-bgpd.sh /usr/local/libexec/consul-checks
+chmod 0755 /usr/local/libexec/consul-checks/*
+rm -f /tmp/efw-bgpd.sh
 
 rcctl enable consul
 rcctl set consul flags "-config-dir=/etc/consul.d -bind=$(dirname ${TRANSIT_IP})"
