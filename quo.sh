eb604a8ccab7ebd9043cc25fcbc3f74a589e4169
RJ Bergeron <rbergero@gmail.com>
2017-06-06 01:29:11 -0400

    switch to opensuse rolling jeos for quo

--- quo.sh
+++ quo.sh
@@ -1,66 +1,63 @@
 #!/bin/bash
 
-SOURCE_ZIP="https://downloads.raspberrypi.org/raspbian_lite_latest"
+# gpg --recv-keys 0xB88B2FD43DBDC284
 
-sum=$(sha256sum "raspbian.zip"|cut -d ' ' -f1) 2>/dev/null
-#2017-04-10
-if [ "${sum}" != "ada1e462ecfb0ef1375c26473bd874a3471c7b3fc367242ad08fb3a73c0895d9" ] ; then
- curl -L "${SOURCE_ZIP}" -o "raspbian.zip"
+SOURCE="http://download.opensuse.org/ports/armv6hl/tumbleweed/images/openSUSE-Tumbleweed-ARM-JeOS-raspberrypi.armv6l-Current.raw.xz"
+SUM="http://download.opensuse.org/ports/armv6hl/tumbleweed/images/openSUSE-Tumbleweed-ARM-JeOS-raspberrypi.armv6l-Current.raw.xz.sha256"
+
+curl -LO "${SUM}"
+
+gpg --verify "$(basename ${SUM})"
+if [ $? -ne 0 ] ; then echo "uh" ; exit 1 ; fi
+
+if [ ! -e "$(basename ${SOURCE})" ] ; then
+  curl -LO "${SOURCE}"
+fi
+
+storedsum=$(grep "$(basename ${SOURCE} -Current.raw.xz)" "$(basename ${SUM})"|cut -d' ' -f1)
+sum=$(sha256sum "$(basename ${SOURCE})"|cut -d ' ' -f1) 2>/dev/null
+if [ "${sum}" != "${storedsum}" ] ; then
+  exit 2
 fi
 
 set -e
 
-image=$(zipinfo -1 "raspbian.zip")
-
-unzip -o "raspbian.zip"
-
 mkdir -p output-qemu-quo/workfiles
 
-mv -f "${image}" output-qemu-quo/system.img
+xzcat "$(basename ${SOURCE})" > output-qemu-quo/system.img
+diskid=$(sfdisk -l "output-qemu-quo/system.img" | awk -F': ' '$1 ~ "Disk identifier" { print $2 }')
+diskid=${diskid:2}
+
 truncate -s4G "output-qemu-quo/system.img"
 
 parted "output-qemu-quo/system.img" resizepart 2 100%
 
-# grab the UUID of the FS volumes as we're about to rewrite /etc/fstab
-fsuuid=$(guestfish --ro -a "output-qemu-quo/system.img" run : blkid /dev/sda2 | awk -F: '$1 == "UUID" { print $2 }')
-fsuuid=${fsuuid//[[:space:]]}
-bootuuid=$(guestfish --ro -a "output-qemu-quo/system.img" run : blkid /dev/sda1 | awk -F: '$1 == "UUID" { print $2 }')
-bootuuid=${bootuuid//[[:space:]]}
+# this crazy thing patches the partition uuid pack to the clone
+newdisk_hex=$(dd if=output-qemu-quo/system.img bs=512 count=1 2> /dev/null | xxd -s +432 | head -n1)
+dm_hex=""
+dcount=0
+for w in $newdisk_hex ; do
+  ((dcount++))
+  case $dcount in
+    6)
+      w=${diskid:6:2}${diskid:4:2}
+      ;;
+    7)
+      w=${diskid:2:2}${diskid:0:2}
+      ;;
+    *)
+      :
+      ;;
+  esac
+  dm_hex="${dm_hex}${w} "
+done
 
-cat > output-qemu-quo/workfiles/fstab <<_EOF_
-proc            /proc           proc    defaults          0       0
-UUID=${bootuuid}  /boot           vfat    defaults          0       2
-UUID=${fsuuid}  /               ext4    defaults,noatime  0       1
-_EOF_
+echo "${dm_hex}" | xxd -r - output-qemu-quo/system.img
 
-cat > output-qemu-quo/workfiles/cmdline.txt <<_EOF_
-dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait
-_EOF_
-
-# ssh _user_ keys
-rm -f output-qemu-quo/workfiles/ssh_user_rsa_key*
-ssh-keygen -f output-qemu-quo/workfiles/ssh_user_rsa_key -N '' -t rsa
-
-cp output-qemu-quo/workfiles/ssh_user_rsa_key.pub output-qemu-quo/workfiles/authorized_keys
-
-# (re)generate ssh host keys as raspbian...doesn't exactly like the qemu machine handed it.
-rm -f output-qemu-quo/workfiles/ssh_host_rsa_key*
-ssh-keygen -f output-qemu-quo/workfiles/ssh_host_rsa_key -N '' -t rsa
-
-# make guestfish mangle the image pre-boot
-# drive guestfish about, modifying a raspbian image to do what we want on boot.
 guestfish <<_EOF_
 add output-qemu-quo/system.img
 run
 resize2fs /dev/sda2
-mount /dev/sda2 /
-mount /dev/sda1 /boot
-copy-in output-qemu-quo/workfiles/fstab /etc
-mkdir /home/pi/.ssh
-copy-in output-qemu-quo/workfiles/authorized_keys /home/pi/.ssh
-copy-in output-qemu-quo/workfiles/ssh_host_rsa_key /etc/ssh
-copy-in output-qemu-quo/workfiles/cmdline.txt /boot
-ln-s /lib/systemd/system/ssh.service /etc/systemd/system/multi-user.target.wants/ssh.service
 _EOF_
 
 curl -L -o output-qemu-quo/workfiles/vmlinuz http://mko.wcs.bbxn.us/fedora/releases/25/Server/armhfp/os/images/pxeboot/vmlinuz
@@ -72,7 +69,7 @@ tmux -L varm new-session -d -s qemu \
   -kernel output-qemu-quo/workfiles/vmlinuz \
   -initrd output-qemu-quo/workfiles/initrd.img \
   -m 1024 -M virt-2.6 -no-reboot \
-  -append "console=ttyAMA0 rw root=UUID=${fsuuid} rootwait rdloaddriver=vfat rd_NO_LVM rd_NO_LUKS rd_NO_MD rd_NO_MDIMSM rd_NO_DM" \
+  -append "console=ttyAMA0 rw root=/dev/vda2 rootwait rdloaddriver=vfat rd_NO_LVM rd_NO_LUKS rd_NO_MD rd_NO_MDIMSM rd_NO_DM net.ifnames=0" \
   -hda output-qemu-quo/system.img \
   -net user,hostfwd=tcp:127.0.0.1:8222-:22 -net nic,model=virtio -nographic
 
