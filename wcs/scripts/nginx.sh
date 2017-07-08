3c08bc9087cfbbe53f2b8bc30a57add8b3d1981f
RJ Bergeron <rbergero@gmail.com>
2017-07-08 01:48:56 -0400

    initial nginx proxy config

--- wcs/scripts/nginx.sh
+++ wcs/scripts/nginx.sh
@@ -12,6 +12,18 @@ chmod 0750 /usr/local/sbin/nginxdir.sh
 cp /tmp/nginxdir.unit /etc/systemd/system/nginxdir.service
 sed -i -e 's@$status@$status/$upstream_cache_status@' /etc/nginx/nginx.conf
 printf 'proxy_cache_path /srv/nginx-cache levels=1:2 keys_zone=wcs_cache:10m max_size=20g inactive=14d use_temp_path=off;\n' > /etc/nginx/conf.d/proxypath.conf
+printf 'upstream mirrors.kernel.org {\nserver mirrors.kernel.org:443;\n}\n' > /etc/nginx/conf.d/upstream-mirrors.kernel.org.conf
+{
+  printf 'proxy_ssl_trusted_certificate /etc/pki/tls/certs/ca-bundle.crt;\nproxy_ssl_verify_depth 3;\nproxy_ssl_verify on;\n'
+  printf 'proxy_ssl_session_reuse on;\nproxy_http_version 1.1;\n'
+} > /etc/nginx/default.d/01-proxy-ssl.conf
+{
+  printf 'slice 5m;\nproxy_cache_key $uri$is_args$args$slice_range;\nproxy_set_header Range $slice_range;\nproxy_cache_revalidate on;\n'
+  printf 'proxy_cache_use_stale error timeout updating;\nadd_header X-Cache-Status $upstream_cache_status;\nproxy_intercept_errors on;\n'
+  printf 'proxy_cache wcs_cache;\nproxy_cache_lock on;\nproxy_cache_valid 200 206 8h;\nproxy_cache_valid any 5m;\n'
+} > /etc/nginx/default.d/00-proxy-cache.conf
+
+nginx -t
 
 mkdir -p /etc/systemd/system/nginx.service.requires
 ln -s /etc/systemd/system/nginxdir.service /etc/systemd/system/nginx.service.requires/nginxdir.service
