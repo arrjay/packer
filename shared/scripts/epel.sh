07312984c13a9b994e196396e994cad1665ec6d7
RJ Bergeron <rbergero@gmail.com>
2017-07-07 21:03:15 -0400

    fix thinko in epel provisioning script

--- shared/scripts/epel.sh
+++ shared/scripts/epel.sh
@@ -9,7 +9,7 @@ if [ -z "${EPEL_MIRROR}" ] ; then
   echo  "EPEL_MIRROR not set, not rewiring EPEL repo"
 else
   set -e
-  printf '[epel]\nbaseurl=%s/$releasever/$basearch/\ngpgcheck=1\n' $MIRROR > /etc/yum.repos.d/epel.repo
+  printf '[epel]\nbaseurl=%s/$releasever/$basearch/\ngpgcheck=1\n' "${EPEL_MIRROR}" > /etc/yum.repos.d/epel.repo
 fi
 
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
