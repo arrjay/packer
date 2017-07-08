1501fb14d8b8dc3f2c509ee34293d8fe33e47562
RJ Bergeron <rbergero@gmail.com>
2017-07-07 20:38:54 -0400

    fix epel sourcing

--- shared/scripts/epel.sh
+++ shared/scripts/epel.sh
@@ -4,12 +4,12 @@ set -e
 yum -y install epel-release
 
 set +e
-if [ -z "${MIRROR}" ] ; then
+if [ -z "${EPEL_MIRROR}" ] ; then
   set -e
-  echo  "MIRROR not set, not rewiring EPEL repo"
+  echo  "EPEL_MIRROR not set, not rewiring EPEL repo"
 else
   set -e
-  printf '[epel]\nbaseurl=%s/fedora-epel/$releasever/$basearch/\ngpgcheck=1\n' $MIRROR > /etc/yum.repos.d/epel.repo
+  printf '[epel]\nbaseurl=%s/$releasever/$basearch/\ngpgcheck=1\n' $MIRROR > /etc/yum.repos.d/epel.repo
 fi
 
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
