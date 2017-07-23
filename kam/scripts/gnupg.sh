4687074306601ee3bab2e0bade049c4c4025d106
RJ Bergeron <rbergero@gmail.com>
2017-07-23 14:04:01 -0400

    update kam to build in latest environs

--- kam/scripts/gnupg.sh
+++ kam/scripts/gnupg.sh
@@ -21,7 +21,7 @@ rcctl enable pcscd
 
 mkdir -p /usr/local/src
 mkdir -p /usr/local/dist
-ftp -o /usr/local/dist/npth-1.3.tar.bz2 ${GNUPG_MIRROR}/ftp/gcrypt/npth/npth-1.3.tar.bz2
+ftp -o /usr/local/dist/npth-1.3.tar.bz2 ${GNUPG_MIRROR}/npth/npth-1.3.tar.bz2
 sha512=$(sha512 /usr/local/dist/npth-1.3.tar.bz2 | cut -d= -f2)
 set +e
 if [ "${sha512}" != " 97b0278cc9448adb42c4a83b8e7bafeed939acaf3dd3a201a1b103df4e48f24224d4bdaeb97903ad1884914ce363cbceffe948a7c1db4f19abf87ca5964f5699" ] ; then
@@ -35,7 +35,7 @@ cd /usr/local/src/npth-1.3
 make
 make install
 
-ftp -o /usr/local/dist/libgpg-error-1.27.tar.bz2 ${GNUPG_MIRROR}/ftp/gcrypt/libgpg-error/libgpg-error-1.27.tar.bz2
+ftp -o /usr/local/dist/libgpg-error-1.27.tar.bz2 ${GNUPG_MIRROR}/libgpg-error/libgpg-error-1.27.tar.bz2
 sha512=$(sha512 /usr/local/dist/libgpg-error-1.27.tar.bz2 | cut -d= -f2)
 set +e
 if [ "${sha512}" != " 51b313c1159074fdbbce84f63bd8afd84b3b58cd608714865b25bed84c1862d050708aa06ac3dab92f1906593df5121161e594c2809653b0fb2c236cae5dcc2f" ] ; then
@@ -49,7 +49,7 @@ cd /usr/local/src/libgpg-error-1.27
 make
 gmake install
 
-ftp -o /usr/local/dist/libksba-1.3.5.tar.bz2 ${GNUPG_MIRROR}/ftp/gcrypt/libksba/libksba-1.3.5.tar.bz2
+ftp -o /usr/local/dist/libksba-1.3.5.tar.bz2 ${GNUPG_MIRROR}/libksba/libksba-1.3.5.tar.bz2
 sha512=$(sha512 /usr/local/dist/libksba-1.3.5.tar.bz2 | cut -d= -f2)
 set +e
 if [ "${sha512}" != " 60179bfd109b7b4fd8d2b30a3216540f03f5a13620d9a5b63f1f95788028708a420911619f172ba57e945a6a2fcd2ef7eaafc5585a0eb2b9652cfadf47bf39a2" ] ; then
@@ -63,7 +63,7 @@ cd /usr/local/src/libksba-1.3.5
 make
 make install
 
-ftp -o /usr/local/dist/libassuan-2.4.3.tar.bz2 ${GNUPG_MIRROR}/ftp/gcrypt/libassuan/libassuan-2.4.3.tar.bz2
+ftp -o /usr/local/dist/libassuan-2.4.3.tar.bz2 ${GNUPG_MIRROR}/libassuan/libassuan-2.4.3.tar.bz2
 sha512=$(sha512 /usr/local/dist/libassuan-2.4.3.tar.bz2 | cut -d= -f2)
 set +e
 if [ "${sha512}" != " 2b0f58682b408fc58fa0ec2980b36e54ba66701bf504cf6c98ec652af43501bc7c18573bc78c5b83260f5a3bdb0ec8f4e0662bafd9bba3fe7287e77598e8e4c1" ] ; then
@@ -78,7 +78,7 @@ patch -p1 < /tmp/libassuan-assuan-socket.c.patch
 make
 make install
 
-ftp -o /usr/local/dist/libgcrypt-1.7.6.tar.bz2 ${GNUPG_MIRROR}/ftp/gcrypt/libgcrypt/libgcrypt-1.7.6.tar.bz2
+ftp -o /usr/local/dist/libgcrypt-1.7.6.tar.bz2 ${GNUPG_MIRROR}/libgcrypt/libgcrypt-1.7.6.tar.bz2
 sha512=$(sha512 /usr/local/dist/libgcrypt-1.7.6.tar.bz2 | cut -d= -f2)
 set +e
 if [ "${sha512}" != " fb7e20c50280f2ca715c3fc9a457f1cc22224797812f8dfa3ec756471bd0049c2cf75ffe12daa543aefe6cdcd1b90b4b9f943f148c073ad99d3a7dee42a8173f" ] ; then
@@ -95,7 +95,7 @@ make install
 # gnupg wants gcc 4.6 or newer?
 export CC=egcc
 
-ftp -o /usr/local/dist/gnupg-2.1.19.tar.bz2 ${GNUPG_MIRROR}/ftp/gcrypt/gnupg/gnupg-2.1.19.tar.bz2
+ftp -o /usr/local/dist/gnupg-2.1.19.tar.bz2 ${GNUPG_MIRROR}/gnupg/gnupg-2.1.19.tar.bz2
 sha512=$(sha512 /usr/local/dist/gnupg-2.1.19.tar.bz2 | cut -d= -f2)
 set +e
 if [ "${sha512}" != " c6d0a2cb7f1f7ce851729559edab08d2356dffe00ee836fc1d71eb4c4e34b566e214a0352934d2985fb0183b9e7ecc1221422d258f3bd467e735c0a5c8a3d0ca" ] ; then
@@ -112,7 +112,7 @@ make install
 
 unset CC
 
-ftp -o /usr/local/dist/gnupg-1.4.21.tar.bz2 ${GNUPG_MIRROR}/ftp/gcrypt/gnupg/gnupg-1.4.21.tar.bz2
+ftp -o /usr/local/dist/gnupg-1.4.21.tar.bz2 ${GNUPG_MIRROR}/gnupg/gnupg-1.4.21.tar.bz2
 sha512=$(sha512 /usr/local/dist/gnupg-1.4.21.tar.bz2 | cut -d= -f2)
 set +e
 if [ "${sha512}" != " 619e0fbc10310c7e55d129027e2945791fe91a0884b1d6f53acb4b2e380d1c6e71d1a516a59876182c5c70a4227d44a74ceda018c343b5291fa9a5d6de77c984" ] ; then
@@ -128,7 +128,7 @@ make install
 
 printf '\nPATH=${PATH}:/usr/local/gnupg14/bin\nexport PATH\n' >> /root/.profile
 
-ftp -o /usr/local/dist/pinentry-1.0.0.tar.bz2 ${GNUPG_MIRROR}/ftp/gcrypt/pinentry/pinentry-1.0.0.tar.bz2
+ftp -o /usr/local/dist/pinentry-1.0.0.tar.bz2 ${GNUPG_MIRROR}/pinentry/pinentry-1.0.0.tar.bz2
 sha512=$(sha512 /usr/local/dist/pinentry-1.0.0.tar.bz2 | cut -d= -f2)
 set +e
 if [ "${sha512}" != " f109236707c51871b5020ef807a551366461fafcfbe09bf8cda19d4b163a42cf622562b905ceb41429f1d648b3f3d27807538709da6a135b67f9888709eccd62" ] ; then
--- kam/scripts/libgfshare.sh
+++ kam/scripts/libgfshare.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir -p /usr/local/src
 mkdir -p /usr/local/dist
-ftp -o /usr/local/dist/libgfshare-2.0.0.tar.gz ${GFSHARE_MIRROR}/files/libgfshare/libgfshare-2.0.0.tar.gz
+ftp -o /usr/local/dist/libgfshare-2.0.0.tar.gz ${GFSHARE_MIRROR}/libgfshare-2.0.0.tar.gz
 sha512=$(sha512 /usr/local/dist/libgfshare-2.0.0.tar.gz | cut -d= -f2)
 set +e
 if [ "${sha512}" != " ead678e3f791c8eb2179e4eacfbad1552bf62be29b7758b3f50e40fdac1efc63f713a0bb03b0e70e8b0fe8c72bf920fb6a44baf6064c74a8bd31194e30dae525" ] ; then
